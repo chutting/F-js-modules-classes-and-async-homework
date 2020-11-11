2069b0df013a7e44b156cf5b85d2f913405dc383
chutting <tingting.chu@thoughtworks.com>
2020-11-11 14:21:23 +0800

    complete practices

--- src/practice1/utils.js
+++ src/practice1/utils.js
@@ -1,1 +1,7 @@
-const getURL
+import {ADDRESS, PORT, PATH} from './constant';
+
+const getURL = (address, port, path) => {
+    return `${address}:${port}${path}`;
+}
+
+export default getURL(ADDRESS, PORT, PATH);
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,16 @@
-export const getPoetry
+// export const getPoetry = async () => {
+//     try {
+//         const response = await fetch("https://v1.jinrishici.com/all.json");
+//         const data = await response.json();
+//         return [data.origin, data.author, data.content];
+//     } catch (e) {
+//         return new Error('request failed');
+//     }
+// }
+
+export const getPoetry = () => {
+    return fetch("https://v1.jinrishici.com/all.json", 'GET')
+    .then(response => response.json())
+    .then(data => [data.origin, data.author, data.content])
+    .catch(err => new Error(err));
+}
